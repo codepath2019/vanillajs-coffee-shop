8ef007777d0b53b7ef3ce762c5580e92f25e6c24
Charles Magic Wooper <codepath2019@gmail.com>
2024-11-13 14:42:18 -0800

    Add project scaffold for index.html, and app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// Wait for DOM is ready for manipulation before execution of app script
+window.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector("nav");
+  console.log(nav);
+});
